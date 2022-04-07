aabb1daab382b84556f2b80a6df249787cce1caa
shivkant kumar <95178381+golu098@users.noreply.github.com>
2022-04-07 13:48:44 +0530

    Create convert feet to meter.cpp

---
+++ convert feet to meter.cpp
@@ -0,0 +1,17 @@
+// created by shiv kant kumar
+
+#include<iostream>
+using namespace std;    
+
+int main() 
+{
+    float meter,feet;
+
+    cout<< "Enter feet : ";
+    cin >> feet ;    
+
+    meter = feet / 3.2808399;
+    cout<< meter ;
+
+    return 0;
+}
