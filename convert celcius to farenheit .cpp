e8164469e1aa87fb846fd1e4c435415e18188c91
shivkant kumar <95178381+golu098@users.noreply.github.com>
2022-04-07 13:49:47 +0530

    Create convert celcius to farenheit .cpp

---
+++ convert celcius to farenheit .cpp
@@ -0,0 +1,17 @@
+// created by shiv kant kumar
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     float celsius, fahrenheit;
+    
+     cout << "Enter Temperature in Celsius : ";
+     cin >> celsius;
+    
+     fahrenheit = (1.8 * celsius) + 32; // Formula to caltulate Celsius to Fahrenheit
+     cout << "Temperature in Fahrenheit : " << fahrenheit;
+    
+     return 0;
+}
