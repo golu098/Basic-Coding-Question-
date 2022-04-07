05b36ff6659c46c931adf132ded062324103c6a7
shivkant kumar <95178381+golu098@users.noreply.github.com>
2022-04-07 13:55:06 +0530

    Create Area of Circle .cpp

---
+++ Area of Circle .cpp
@@ -0,0 +1,17 @@
+// created by shiv kant kumar
+
+#include<iostream>
+#define PI 3.141
+using namespace std;
+
+int main()
+{
+    float radius, area;
+    cout << "Enter Radius: ";
+    
+    cin >> radius;
+    area = PI * radius * radius;
+    
+    cout << area;
+    return 0;
+}
