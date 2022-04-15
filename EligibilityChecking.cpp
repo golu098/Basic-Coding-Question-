5c272f29914712c0b946d68c1002a12be21e9de2
shivkant kumar <95178381+golu098@users.noreply.github.com>
2022-04-15 22:17:40 +0530

    Add files via upload

---
+++ EligibilityChecking.cpp
@@ -0,0 +1,17 @@
+/* created by shiv kant kumar
+Follow on insta id:- real_warrrior_golu(For any query) 
+*/
+// Question:-  To find the eligibility of admission for an engineering course based on the following criteria
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a>=18){
+        cout<<"Congratulation ! You take admission ";
+    }
+    else{
+        cout<<"you have failed !";
+    }
+ return 0;
+}
