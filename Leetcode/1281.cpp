dd50f85769e8451c14deb182ebf66122b33f04f4
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2022-06-01 03:43:45 +0530

    Create 1281.cpp

---
+++ Leetcode/1281.cpp
@@ -0,0 +1,27 @@
+/* Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+*/
+
+
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod=1;
+        int sum=0;
+        while(n!=0){
+            int digit =n%10;
+            prod=prod*digit;
+            sum=sum+digit;
+            n=n/10;
+            
+        }
+        int ans=prod-sum;
+        return ans;
+    }
+};
